eef23e6b542c70f7e9ac4ea5ec61e06c23ca95e0
Danut Gavrus <danut.gavrus@gmail.com>
2023-01-30 13:11:22 +0200

    [FEATURE] Search functionality in real time.

--- src/App.js
+++ src/App.js
@@ -5,126 +5,137 @@ import { auth, signIn } from "./scripts/Firebase.js";
 import { useAuthState } from "react-firebase-hooks/auth";
 import Loading from "./components/Loading";
 import SignIn from "./pages/SignIn";
+import { useState } from "react";
+import { useEffect } from "react";
 
 function App() {
   const [user, loading] = useAuthState(auth);
+  const [categoriesList, setCategoriesList] = useState([]);
+  const [messagesList, setMessagesList] = useState([]);
+
+  // TODO: Get the categories and the messages from Firebase
+  useEffect(() => {
+    setCategoriesList([
+      {
+        id: 1,
+        unicode: "☕",
+        title: "Coffee and stories",
+        description: "General topic for any subject out there."
+      },
+      {
+        id: 2,
+        unicode: "🏕️",
+        title: "Outdoor activities",
+        description: "Feel like discussing about an outside activity?"
+      },
+      {
+        id: 3,
+        unicode: "🎮",
+        title: "Gaming",
+        description: "Just gamers speaking about games."
+      },
+      {
+        id: 4,
+        unicode: "📗",
+        title: "Book club",
+        description: "Share something interesting you've read?."
+      },
+      {
+        id: 5,
+        unicode: "📻",
+        title: "Music club",
+        description: "Tell others about the best sounds you've heard."
+      }
+    ]);
+
+    setMessagesList([
+      {
+        id: 1,
+        content: "Salut!",
+        user: "Vlad Avram",
+        tempIcon: "🎮"
+      },
+      {
+        id: 2,
+        content: "Salut.",
+        user: "Danut Gavrus",
+        tempIcon: "📻"
+      },
+      {
+        id: 3,
+        content: "Ce faci?",
+        user: "Vlad Avram",
+        tempIcon: "🎮"
+      },
+      {
+        id: 4,
+        content: "Bine, beau o cafelutsa. Tu ce faci?",
+        user: "Danut Gavrus",
+        tempIcon: "📻"
+      },
+      {
+        id: 5,
+        content: "Da bine, stateam.",
+        user: "Vlad Avram",
+        tempIcon: "🎮"
+      },
+      {
+        id: 6,
+        content: "Cred ca ma duc sa-mi fac si eu o cafelutsa acum ca ai zis.",
+        user: "Vlad Avram",
+        tempIcon: "🎮"
+      },
+      {
+        id: 7,
+        content: "Ba",
+        user: "Danut Gavrus",
+        tempIcon: "📻"
+      },
+      {
+        id: 8,
+        content: "Bine faci :))",
+        user: "Danut Gavrus",
+        tempIcon: "📻"
+      },
+      {
+        id: 9,
+        content: "mda",
+        user: "Vlad Avram",
+        tempIcon: "🎮"
+      },
+      {
+        id: 10,
+        content: "Cf pe seara? Ma gandeam sa te intreb ca sa ceva motiv, asta e doar un text de umplutura pentru a avea un mesaj mai lung.",
+        user: "Vlad Avram",
+        tempIcon: "🎮"
+      },
+      {
+        id: 11,
+        content: "Hmm",
+        user: "Danut Gavrus",
+        tempIcon: "📻"
+      },
+      {
+        id: 12,
+        content: "Trebuia sa ies cu cineva",
+        user: "Danut Gavrus",
+        tempIcon: "📻"
+      },
+      {
+        id: 13,
+        content: "Da nu-i sigura treaba. Text de umplutura ca sa facem un mesaj foarte lung ca sa ii vedem style-ul. Mai trebuie niste text ca sa depasim lungimea maxima a unui rand. Da nu-i sigura treaba. Text de umplutura ca sa facem un mesaj foarte lung ca sa ii vedem style-ul. Mai trebuie niste text ca sa depasim lungimea maxima a unui rand.",
+        user: "Danut Gavrus",
+        tempIcon: "📻"
+      }
+    ]);
+  }, []);
 
   const router = createBrowserRouter(createRoutesFromElements(
     <Route path="/" element={<RootLayout displayName={user && user.displayName} />}>
       <Route index element={<>
         {loading && <Loading />}
         {!loading && !user && <SignIn signIn={signIn} />}
-        {!loading && user && <LiveChat categoriesList={[
-          {
-            id: 1,
-            unicode: "☕",
-            title: "Coffee and stories",
-            description: "General topic for any subject out there."
-          },
-          {
-            id: 2,
-            unicode: "\🏕️",
-            title: "Outdoor activities",
-            description: "Feel like discussing about an outside activity?"
-          },
-          {
-            id: 3,
-            unicode: "🎮",
-            title: "Gaming",
-            description: "Just gamers speaking about games."
-          },
-          {
-            id: 4,
-            unicode: "📗",
-            title: "Book club",
-            description: "Share something interesting you've read?."
-          },
-          {
-            id: 5,
-            unicode: "📻",
-            title: "Music club",
-            description: "Tell others about the best sounds you've heard."
-          }
-        ]} messagesList={[
-          {
-            id: 1,
-            content: "Salut!",
-            user: "Vlad Avram",
-            tempIcon: "🎮"
-          },
-          {
-            id: 2,
-            content: "Salut.",
-            user: "Danut Gavrus",
-            tempIcon: "📻"
-          },
-          {
-            id: 3,
-            content: "Ce faci?",
-            user: "Vlad Avram",
-            tempIcon: "🎮"
-          },
-          {
-            id: 4,
-            content: "Bine, beau o cafelutsa. Tu ce faci?",
-            user: "Danut Gavrus",
-            tempIcon: "📻"
-          },
-          {
-            id: 5,
-            content: "Da bine, stateam.",
-            user: "Vlad Avram",
-            tempIcon: "🎮"
-          },
-          {
-            id: 6,
-            content: "Cred ca ma duc sa-mi fac si eu o cafelutsa acum ca ai zis.",
-            user: "Vlad Avram",
-            tempIcon: "🎮"
-          },
-          {
-            id: 7,
-            content: "Ba",
-            user: "Danut Gavrus",
-            tempIcon: "📻"
-          },
-          {
-            id: 8,
-            content: "Bine faci :))",
-            user: "Danut Gavrus",
-            tempIcon: "📻"
-          },
-          {
-            id: 9,
-            content: "mda",
-            user: "Vlad Avram",
-            tempIcon: "🎮"
-          },
-          {
-            id: 10,
-            content: "Cf pe seara? Ma gandeam sa te intreb ca sa ceva motiv, asta e doar un text de umplutura pentru a avea un mesaj mai lung.",
-            user: "Vlad Avram",
-            tempIcon: "🎮"
-          },
-          {
-            id: 11,
-            content: "Hmm",
-            user: "Danut Gavrus",
-            tempIcon: "📻"
-          },
-          {
-            id: 12,
-            content: "Trebuia sa ies cu cineva",
-            user: "Danut Gavrus",
-            tempIcon: "📻"
-          },
-          {
-            id: 13,
-            content: "Da nu-i sigura treaba. Text de umplutura ca sa facem un mesaj foarte lung ca sa ii vedem style-ul. Mai trebuie niste text ca sa depasim lungimea maxima a unui rand. Da nu-i sigura treaba. Text de umplutura ca sa facem un mesaj foarte lung ca sa ii vedem style-ul. Mai trebuie niste text ca sa depasim lungimea maxima a unui rand.",
-            user: "Danut Gavrus",
-            tempIcon: "📻"
-          }
-        ]} />}</>}>
+        {!loading && user && <LiveChat categoriesList={categoriesList} messagesList={messagesList} />}</>}>
       </Route>
     </Route >
   ))
--- src/components/CategoriesList.js
+++ src/components/CategoriesList.js
@@ -1,14 +1,35 @@
-import { cloneElement } from "react";
+import { cloneElement, useCallback, useState } from "react";
 import { useRef, useEffect } from "react";
 import Category from "../components/Category";
 
 export default function CategoriesList({ categoriesList, setChatHeader }) {
+  // TODO: Add clear icon to categories search bar
   // TOOD: Update messages based on the current category
   const scrollDownBtn = useRef();
   const lastCategoryRef = useRef();
   const scrollUpBtn = useRef();
   const firstCategoryRef = useRef();
   const categoriesListRef = useRef();
+  const searchBarRef = useRef();
+
+  const [searchText, setSearchText] = useState("");
+
+  const manageScrollBtnsVisibility = useCallback(() => {
+    // Categories overflow
+    if (categoriesListRef.current?.scrollHeight > categoriesListRef.current?.clientHeight) {
+      // Scrolled to bottom
+      if (categoriesListRef.current?.scrollHeight - Math.ceil(categoriesListRef.current?.scrollTop) <= categoriesListRef.current?.clientHeight) {
+        toggleScrollBtnVisibility("bottom");
+      }
+      else if (categoriesListRef.current?.scrollTop === 0) {
+        toggleScrollBtnVisibility("top");
+      }
+    }
+    else {
+      toggleScrollBtnVisibility("none");
+    }
+    return true;
+  }, []);
 
   useEffect(() => {
     manageScrollBtnsVisibility();
@@ -17,52 +38,50 @@ export default function CategoriesList({ categoriesList, setChatHeader }) {
     return () => {
       window.removeEventListener("resize", manageScrollBtnsVisibility);
     };
-  }, []);
+  }, [manageScrollBtnsVisibility, searchText]);
 
-  const manageScrollBtnsVisibility = () => {
-    // Scrolled to bottom
-    if (categoriesListRef.current?.scrollHeight - Math.ceil(categoriesListRef.current?.scrollTop) <= categoriesListRef.current?.clientHeight) {
-      scrollDownBtn.current?.classList.add("hidden");
-      scrollUpBtn.current?.classList.remove("hidden");
+  const toggleScrollBtnVisibility = (position) => {
+    if (position === "bottom") {
+      scrollDownBtn.current?.classList.add("invisible");
+      scrollUpBtn.current?.classList.remove("invisible");
     }
-
-    // Scrolled to top
-    if (categoriesListRef.current?.scrollTop === 0) {
-      scrollUpBtn.current?.classList.add("hidden");
-      scrollDownBtn.current?.classList.remove("hidden");
+    else if (position === "top") {
+      scrollUpBtn.current?.classList.add("invisible");
+      scrollDownBtn.current?.classList.remove("invisible");
     }
-  }
+    else if (position === "none") {
+      scrollUpBtn.current?.classList.add("invisible");
+      scrollDownBtn.current?.classList.add("invisible");
+    }
+  };
 
-  const scrollCategoriesDown = () => {
+  const scrollCategoriesBottom = () => {
+    toggleScrollBtnVisibility("bottom");
     lastCategoryRef.current?.scrollIntoView({ behavior: "smooth" });
-    scrollDownBtn.current?.classList.add("hidden");
-    scrollUpBtn.current?.classList.remove("hidden");
-  }
+  };
 
-  const scrollCategoriesUp = () => {
+  const scrollCategoriesTop = () => {
+    toggleScrollBtnVisibility("top");
     firstCategoryRef.current?.scrollIntoView({ behavior: "smooth" });
-    scrollUpBtn.current?.classList.add("hidden");
-    scrollDownBtn.current?.classList.remove("hidden");
-  }
+  };
 
   return (
     <div className="flex flex-col overflow-y-auto">
-      {/* TODO: Search categories functionality */}
-      <input type="text" placeholder="Search categories ..." className="font-serif rounded-xl mx-3 my-6 px-3 py-1 box-border border border-transparent focus:outline-none focus:border-[var(--color-accent)]" />
-      <button ref={scrollUpBtn} onClick={scrollCategoriesUp} className="hidden">{"↑"}</button>
+      <input ref={searchBarRef} type="text" placeholder="Search categories ..." onChange={(e) => { setSearchText(e.target.value) }} className="font-serif rounded-xl mx-3 mt-6 px-3 py-1 box-border border border-transparent focus:outline-none focus:border-[var(--color-accent)]" />
+      <button ref={scrollUpBtn} onClick={scrollCategoriesTop} className="h-6 invisible">{"↑"}</button>
       <ul ref={categoriesListRef} onScroll={manageScrollBtnsVisibility} className="scrollbar-hide overflow-y-scroll">
-        {categoriesList?.map((category, i, { length }) => {
+        {categoriesList?.filter(category => category.title.toLowerCase().includes(searchText.toLowerCase())).map((category, i, { length }) => {
           let categoryComponent = <Category key={category.id} category={category} setChatHeader={setChatHeader} />;
           if (i === 0) {
             categoryComponent = cloneElement(categoryComponent, { innerRef: firstCategoryRef });
           }
           if (i === length - 1) {
-            categoryComponent = cloneElement(categoryComponent, { innerRef: lastCategoryRef });
+            categoryComponent = cloneElement(categoryComponent, { innerRef: lastCategoryRef, last: true });
           }
           return categoryComponent;
         })}
       </ul>
-      <button ref={scrollDownBtn} onClick={scrollCategoriesDown} className="hidden">{"↓"}</button>
+      <button ref={scrollDownBtn} onClick={scrollCategoriesBottom} className="h-6 invisible">{"↓"}</button>
     </div>
-  )
+  );
 }
