b0d28883589a203917f3575bb6954c3dc186c17d
Danut Gavrus <danut.gavrus@gmail.com>
2023-02-16 20:36:03 +0200

    fix

--- src/layouts/RootLayout.js
+++ src/layouts/RootLayout.js
@@ -19,6 +19,7 @@ export default function RootLayout(context) {
         <div className="w-11 h-6 bg-gray-200 rounded-full dark:bg-gray-700 peer-checked:after:translate-x-full   after:absolute after:top-[2px] after:left-[2px] after:bg-white dark:after:bg-black after:border-gray-300 dark:after:border-gray-700 after:border after:rounded-full after:h-5 after:w-5 after:transition-all  dark:peer-checked:bg-secondary" />
         <span className="ml-1 text-xs text-accent font-bold">Dark mode</span>
       </label>
+
       <Outlet context={context} />
     </div >
   );
