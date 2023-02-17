1f75e6037e0a3ca9e96323d3ca359e80eafa6a0c
Danut Gavrus <danut.gavrus@gmail.com>
2023-02-17 13:26:35 +0200

    [FEATURE] Sign in with Apple option.

--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -1,17 +1,23 @@
-import { GoogleAuthProvider, getAuth, signInWithRedirect } from "firebase/auth";
+import { signInWithRedirect, getAuth, GoogleAuthProvider, OAuthProvider } from "firebase/auth";
 import { useOutletContext } from "react-router-dom";
 
 export default function SignIn() {
   const context = useOutletContext();
   const app = context?.app;
 
-  const handleSignIn = () => {
+  const handleSignInWGoogle = () => {
     signInWithRedirect(getAuth(app), new GoogleAuthProvider());
   }
 
+  const handleSignInWApple = () => {
+    signInWithRedirect(getAuth(app), new OAuthProvider());
+  }
+
   return (
-    <p className="text-2xl md:text-3xl text-center">👋 Hi, please <button onClick={handleSignIn} className="font-bold text-accent underline">sign in</button> with Google to use this live-chat! 👋</p>
+    <>
+      <h1 className="text-2xl text-center">👋 Hi, please <span className="font-bold text-accent">sign in</span> to use this live-chat! 👋</h1>
+      <button onClick={handleSignInWGoogle} className="text-lg text-accent underline">Sign in with <span className="font-bold">Google</span></button>
+      <button onClick={handleSignInWApple} className="text-lg text-accent underline">Sign in with <span className="font-bold">Apple</span></button>
+    </>
   );
 }
-
-// TODO: BUG Some iOS users may not login, maybe add another sign-in option?
