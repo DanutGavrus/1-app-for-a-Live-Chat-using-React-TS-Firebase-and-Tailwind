d2ac3e93692e8f249cba79c62d723e7fe3276936
Danut Gavrus <danut.gavrus@gmail.com>
2023-02-28 17:56:40 +0200

    test

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -5,7 +5,7 @@ export default function NotFound() {
     <>
       <h1 className="font-bold text-accent">Page not found!</h1>
       <h3 className="text-center">The specified route was not found on this website. Please check the URL for mistakes and try again.</h3>
-      <p className="mt-5 text-center">Go back to <span className="underline"><Link to="/">Homepage</Link></span>.</p>
+      <p className="mt-5 text-center">Go back to <span className="underline"><Link to="/">Homepage</Link></span>.TEST</p>
     </>
   )
 }
